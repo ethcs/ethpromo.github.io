08f76baf2a74a4d1034ddb212369e20bc1ddbe31
ethcs <40736686+ethcs@users.noreply.github.com>
2018-07-01 21:15:57 +0700

    Add files via upload

---
+++ in(2).php
@@ -0,0 +1,3 @@
+// static33
+var NO_PINGY=1;(function(){var d=new Date;d.setTime(d.getTime()+3600*1000);document.cookie="no_trackyy_101111047=1;expires="+d.toGMTString()+";path=/;";})();
+// exit bad site id
